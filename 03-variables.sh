0907be73f554e2230587694ffdb0d4e3edda96c7
soumya <korlagani.soumya@gmail.com>
2025-09-02 18:00:48 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring(creating) of variable
+PERSON1=Ramesh #no space between = and value
+PERSON2=suresh
+
+#referring(using) variable
+echo "$PERSON1:: Hello $PERSON2,How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am fine too. how is your work"
+echo "$PERSON2:: not bad. I am thinking to upgarde to devops"
