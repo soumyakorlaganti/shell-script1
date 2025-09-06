f2a093a64c0c66e6944059f423854c94279b98ff
soumya <korlagani.soumya@gmail.com>
2025-09-06 06:32:27 +0530

    functions

---
+++ 11-functions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE(){
+    # echo "Exit status: $1"
+    # echo "What are you doing: $2"
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi        
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run this script with root access."
+    exit 1 # manually exit if error comes.
+else 
+    echo "You are super user."
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
---
+++ 14-install-packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi        
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run this script with root access."
+    exit 1 # manually exit if error comes.
+else 
+    echo "You are super user."
+fi
+
+echo "All packages: $@"
+# for i in $@
+# do 
+#     echo "Package to install: $i"
+#     dnf list installed $i &>>$LOGFILE
+#     if [ $? -eq 0 ]
+#     then
+#         echo -e "$i already installed...$Y SKIPPING $N"
+#     else
+          dnf install $i -y &>>$LOGFILE
+          VALIDATE $? "Installation of $i"
+      fi 
+# done
